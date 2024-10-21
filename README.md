33366591812c4da750d94e577598ebd76866f07f
Silvio Rhatto <rhatto@torproject.org>
2024-10-21 15:47:30 -0300

    Feat: initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ansible role for Onionprobe
+
+This [Ansible][] role manages [Onionprobe][] installations.
+
+## Variables
+
+Check [defaults/main.yml](defaults/main.yml) for a commented list of variables.
+
+[Ansible]: https://ansible.com
+[Onionprobe]: https://gitlab.torproject.org/tpo/onion-services/onionprobe
---
+++ tests/integration/test_integration.py
@@ -0,0 +1,18 @@
+"""
+Tests for molecule scenarios.
+
+Documented at
+https://ansible.readthedocs.io/projects/pytest-ansible/getting_started/
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from pytest_ansible.molecule import MoleculeScenario
+
+def test_integration(molecule_scenario: MoleculeScenario) -> None:
+    """Run molecule for each scenario.
+
+    :param molecule_scenario: The molecule scenario object
+    """
+    proc = molecule_scenario.test()
+    assert proc.returncode == 0
